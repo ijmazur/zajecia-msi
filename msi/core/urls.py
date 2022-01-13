25b5a70c951ee6c193778c19241341f65eb8c280
MarBor2000 <73531965+MarBor2000@users.noreply.github.com>
2022-01-13 21:06:40 +0100

    update urls

--- msi/core/urls.py
+++ msi/core/urls.py
@@ -1,19 +1,18 @@
-# from django.urls import include, path
-# from rest_framework import routers
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-# from core import views
-#
-# router = routers.DefaultRouter()
-# # router.register(r'doctors', views.DoctorViewSet)
-# # router.register(r'patients', views.PatientViewSet)
-# # router.register(r'specializations', views.SpecializationViewSet)
-# # router.register(r'visits', views.VisitViewSet)
-# # router.register(r'exam-results', views.ExamResultViewSet)
-#
-# # Wire up our API using automatic URL routing.
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-#     # path('api/mail', views.MailView.send)
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from core import views
+
+router = routers.DefaultRouter()
+router.register(r'dispositors', views.DispositorViewSet)
+router.register(r'drivers', views.DriverViewSet)
+router.register(r'locations', views.LocationViewSet)
+router.register(r'squads', views.SquadViewSet)
+router.register(r'ambulances', views.AmbulanceViewSet)
+router.register(r'ambulance-calls', views.AmbulanceCallViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
