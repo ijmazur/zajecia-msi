cb80aa098cd844cb671f5af1941d49f709fddcbf
vroniu <gwrona1@gmail.com>
2022-01-15 20:25:29 +0100

    added login and authentication

--- frontendzik/src/components/Login.js
+++ frontendzik/src/components/Login.js
@@ -1,7 +1,10 @@
 import './Login.css';
 import authService from '../services/auth.service';
+import { useLocation } from 'react-router';
 
 export const Login = () => {
+    const { state } = useLocation();
+    const { data } = state; // Read values passed on state
 
     const sumbitLogin = (event) => {
         event.preventDefault();
@@ -22,14 +25,14 @@ export const Login = () => {
         <div className='center'>
             <form onSubmit={sumbitLogin}>
                 <div className='form-group'>
-                    <label htmlFor='exampleInputEmail1'>Email address</label>
-                    <input type='text' className='form-control' id='username' placeholder='Enter email'/>
+                    <label htmlFor='exampleInputEmail1'>Username</label>
+                    <input type='text' className='form-control' id='username' placeholder='Enter username'/>
                 </div>
                 <div className='form-group'>
                     <label htmlFor='exampleInputPassword1'>Password</label>
                     <input type='password' className='form-control' id='password' placeholder='Password'/>
                 </div>
-                <button type='submit' className='btn btn-primary'>Submit</button>
+                <button type='submit' className='btn btn-primary'>Log in</button>
             </form>
         </div>
     );
--- msi/core/views.py
+++ msi/core/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse, response
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import permissions
 from core.models import Dispositor, Driver, Location, Squad, Ambulance, AmbulanceCall
 from core.serializers import DispositorSerializer, DriverSerializer, LocationSerializer, SquadSerializer, \
@@ -17,8 +18,8 @@ import json
 
 class DispositorViewSet(viewsets.ModelViewSet):
     queryset = Dispositor.objects.all()
-    authentication_classes = [permissions.IsAuthenticated]
-    # permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = DispositorSerializer
 
 
