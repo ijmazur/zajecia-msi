215c37aeb228dccf323792781cd0d7d4ca63050d
MarBor2000 <73531965+MarBor2000@users.noreply.github.com>
2022-01-22 23:50:45 +0100

    "First test"

--- msi/core/tests.py
+++ msi/core/tests.py
@@ -4,38 +4,10 @@ from rest_framework.test import APITestCase
 from .models import *
 import json
 
-# Create your tests here.
+#Create your tests here.
 
-# class DoctorTests(APITestCase):
-#     def setUp(self):
-#         Specialization.objects.create(name="test specialization")
-#         self.url = 'http://127.0.0.1:8000/'
+class DoctorTests(APITestCase):
 
-#     def test_create_doctor(self):
-#         data = {
-#             "first_name": "Andrzej",
-#             "last_name": "Wiertara",
-#             "date_of_birth": "2000-10-22",
-#             "username": "awiertara2",
-#             "password": "testtesttest",
-#             "mail": "test@test.com",
-#             "specialization": [1]
-#         }
-#         response = self.client.post(self.url + 'doctors/', data, format = 'json')
-#         self.assertEqual(response.status_code, 201)
-#         created_doctor_response = self.client.get(self.url + 'doctors/', format = 'json')
-#         self.assertEqual(created_doctor_response.status_code, 200)
-#         doctor = json.loads(created_doctor_response.content)[0]
-#         self.assertEqual(doctor['username'], 'awiertara2')
-#         self.assertEqual(doctor['specialization'], [1])
+    def test_create_doctor(self):
+        self.assertEqual(True,True)
 
-#     def test_login_as_doctor(self):
-#         data = {
-#             "username": "awiertara2",
-#             "password": "testtesttest"
-#         }
-#         response = self.client.post(self.url + 'api/token/', data, format = 'json')
-#         self.assertEqual(response.status_code, 200)
-#         data = json.loads(response.content)
-#         self.assertIsNotNone(data['access'])
-#         self.assertIsNotNone(data['refresh'])
