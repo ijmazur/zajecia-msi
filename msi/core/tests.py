f121a36beb2559f88edf8093cef273a67dc86545
MarBor2000 <73531965+MarBor2000@users.noreply.github.com>
2022-01-25 21:46:08 +0100

    Create test shoudn't create ambulance with bad options

--- msi/core/tests.py
+++ msi/core/tests.py
@@ -30,4 +30,17 @@ class Ambulance(APITestCase):
         created_ambulance_response = self.client.get(self.url+'ambulances/?option=all', format='json')
         self.assertEqual(created_ambulance_response.status_code,200)
 
+    #shouldnt create ambulance with bad options
+    def test_AmbulanceNO(self):
+        data = {
+            "street": "Leszka",
+            "number": "dwa",
+            "city": "Lublin",
+            "vehicle_name": "BMW",
+            "registration_number": "CROW2137",
+            "status": 0
+        }
+        response = self.client.post(self.url + 'ambulances/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+
 
