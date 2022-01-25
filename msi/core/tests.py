3f1b993d1fa31d9c4eb6d9b4004bb552d14f8ffa
MarBor2000 <73531965+MarBor2000@users.noreply.github.com>
2022-01-25 19:40:38 +0100

    "Create ambualnce test"

--- msi/core/tests.py
+++ msi/core/tests.py
@@ -6,9 +6,22 @@ import json
 
 #Create your tests here.
 
-class DoctorTests(APITestCase):
+class Ambulance(APITestCase):
 
-    #First Test
-    def test_create_doctor(self):
+    #"First Test"
+    def test_test(self):
         self.assertEqual(True,True)
+
+    #create ambulance
+    def test_Ambulance(self):
+        data = {
+            "street": "Leszka",
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name":"BMW",
+            "registration_number":"CROW2137",
+            "status":0
+        }
+        response = self.client.post('http://127.0.0.1:8000/ambulances/',data,format='json')
+        self.assertEqual(response.status_code,201)
 
