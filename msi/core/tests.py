49ce0c5cc6dd21ff7c61e8b2c2deb2480a6abae3
MarBor2000 <73531965+MarBor2000@users.noreply.github.com>
2022-01-31 20:11:21 +0100

    Tests

---
+++ msi/core/tests.py
@@ -0,0 +1,262 @@
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+import json
+
+#Create your tests here
+#Tests for ambualnce
+
+class Ambulance(APITestCase):
+
+
+    def setUp(self):
+        self.url='http://127.0.0.1:8000/'
+
+    #"First Test"
+    def test_test(self):
+        self.assertEqual(True,True)
+
+    #create ambulance
+    def test_Ambulance(self):
+        data = {
+            "street": "Leszka",
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name": "BMW",
+            "registration_number": "CROW2137",
+            "status": 0
+        }
+        response = self.client.post(self.url+'ambulances/',data,format='json')
+        self.assertEqual(response.status_code,201)
+        created_ambulance_response = self.client.get(self.url+'ambulances/?option=all', format='json')
+        self.assertEqual(created_ambulance_response.status_code,200)
+
+    #create test with missing options
+    def test_Ambulance_miss(self):
+        data = {
+            "street": "Leszka",
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name":"BMW",
+            "registration_number":"CROW2137",
+        }
+        response = self.client.post(self.url+'ambulances/',data,format='json')
+        self.assertEqual(response.status_code,400)
+
+
+    #Test busy option in viewset
+    def test_ambulance_view_set_busy(self):
+        data = {
+            "street": "Leszka",
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name":"BMW",
+            "registration_number":"CROW2137",
+            "status":0
+        }
+        response = self.client.post(self.url+'ambulances/',data,format='json')
+        self.assertEqual(response.status_code,201)
+        created_ambulance_response = self.client.get(self.url+'ambulances/?option=busy', format='json')
+        self.assertEqual(created_ambulance_response.status_code,200)
+
+    #Test busy option in viewset
+    def test_ambulance_view_set_busy(self):
+        data = {
+            "street": "Leszka",
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name":"BMW",
+            "registration_number":"CROW2137",
+            "status":0
+        }
+        response = self.client.post(self.url+'ambulances/',data,format='json')
+        self.assertEqual(response.status_code,201)
+        created_ambulance_response = self.client.get(self.url+'ambulances/?option=busy', format='json')
+        self.assertEqual(created_ambulance_response.status_code,200)
+
+    #Test bad option in viewset
+    def test_ambulance_view_set_bad_option(self):
+        data = {
+            "street": "Leszka",
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name":"BMW",
+            "registration_number":"CROW2137",
+            "status":0
+        }
+        response = self.client.post(self.url+'ambulances/',data,format='json')
+        self.assertEqual(response.status_code,201)
+        created_ambulance_response = self.client.get(self.url+'ambulances/?option=UMCS', format='json')
+        self.assertEqual(created_ambulance_response.status_code,400)
+
+    #Test_my_calls_option_in_viewset
+    def test_ambulance_view_set_my_calls(self):
+        data = {
+            "street": "Leszka",
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name":"BMW",
+            "registration_number":"CROW2137",
+            "status":0
+        }
+        response = self.client.post(self.url+'ambulances/',data,format='json')
+        self.assertEqual(response.status_code,201)
+        created_ambulance_response = self.client.get(self.url+'ambulances/?option=my-calls', format='json')
+        self.assertEqual(created_ambulance_response.status_code,400)
+
+
+    #shouldnt create ambulance with blank
+    def test_AmbulanceBlank(self):
+        data = {
+            "number": 2,
+            "city": "Lublin",
+            "vehicle_name": "BMW",
+            "registration_number": "CROW2137",
+            "status": 0
+        }
+        response = self.client.post(self.url + 'ambulances/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    #Tests to check every option
+    #===========================================================================================
+
+    def test_AmbulancestreetBadStreet(self):
+        data = {
+            "street": "Leszka",
+            "number": "dwa",
+            "city": "Lublin",
+            "vehicle_name": "BMW",
+            "registration_number": "CROW2137",
+            "status": 0
+        }
+        response = self.client.post(self.url + 'ambulances/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_AmbulanceBadNumber(self):
+        data = {
+            "street": "Leszka",
+            "number": "dwa",
+            "city": "Lublin",
+            "vehicle_name": "BMW",
+            "registration_number": "CROW2137",
+            "status": 0
+        }
+        response = self.client.post(self.url + 'ambulances/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_AmbulancestreetBadCity(self):
+        data = {
+            "street": "Leszka",
+            "number": "dwa",
+            "city": 10,
+            "vehicle_name": "BMW",
+            "registration_number": "CROW2137",
+            "status": 0
+        }
+        response = self.client.post(self.url + 'ambulances/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_AmbulanceBadNumberBadVehicle_Name(self):
+        data = {
+            "street": "Leszka",
+            "number": "dwa",
+            "city": "Lublin",
+            "vehicle_name": 10,
+            "registration_number": "CROW2137",
+            "status": 0
+        }
+        response = self.client.post(self.url + 'ambulances/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_AmbulancestreetBadStatus(self):
+        data = {
+            "street": "Leszka",
+            "number": "dwa",
+            "city": "Lublin",
+            "vehicle_name": "BMW",
+            "registration_number": "CROW2137",
+            "status": "Free"
+        }
+        response = self.client.post(self.url + 'ambulances/', data, format='json')
+        self.assertEqual(response.status_code, 400)
+    #===========================================================================================
+
+
+#Tests for hospital
+
+class HospitalUser(APITestCase):
+    def setUp(self):
+        self.url='http://127.0.0.1:8000/'
+
+    #create driver
+    def test_driver(self):
+        data = {
+            "first_name": "Marcin",
+            "last_name": "Borkowski",
+            "date_of_birth":"2000-08-31",
+            "username": "SiemaEniu12",
+            "password": "SiemaEniu13",
+            "mail": "siemaeniu@gmail.com"
+        }
+        response = self.client.post(self.url+'drivers/',data,format='json')
+        self.assertEqual(response.status_code,201)
+        created_driver_response = self.client.get(self.url+'drivers/', format='json')
+        self.assertEqual(created_driver_response.status_code,200)
+
+    #shouldnt create driver with bad option
+    def test_driver_bad_option_first_name(self):
+        data = {
+            "first_name":10,
+            "last_name": "Borkowski",
+            "date_of_birth":"2000-08-31",
+            "username": "SiemaEniu12",
+            "password": "SiemaEniu13",
+            "mail": "siemaeniu@gmail.com"
+        }
+        response = self.client.post(self.url+'drivers/',data,format='json')
+        self.assertEqual(response.status_code,400)
+
+    #shouldnt create driver  with bad date of birth option
+    def test_driver_bad_option_date(self):
+        data = {
+            "first_name":"Marcin",
+            "last_name": "Borkowski",
+            "date_of_birth":2000,
+            "username": "SiemaEniu12",
+            "password": "SiemaEniu13",
+            "mail": "siemaeniu@gmail.com"
+        }
+        response = self.client.post(self.url+'drivers/',data,format='json')
+        self.assertEqual(response.status_code,400)
+
+    #shouldnt create driver with blank
+    def test_driver_blank(self):
+        data = {
+            "last_name": "Borkowski",
+            "date_of_birth":"2000-08-31",
+            "username": "SiemaEniu12",
+            "password": "SiemaEniu13",
+            "mail": "siemaeniu@gmail.com"
+        }
+        response = self.client.post(self.url+'drivers/',data,format='json')
+        self.assertEqual(response.status_code,400)
+
+
+    #create driver
+    def test_login_as_driver(self):
+        data = {
+            "first_name": "Marcin",
+            "last_name": "Borkowski",
+            "date_of_birth":"2000-08-31",
+            "username": "SiemaEniu12",
+            "password": "SiemaEniu13",
+            "mail": "siemaeniu@gmail.com"
+        }
+        response = self.client.post(self.url+'drivers/',data,format='json')
+        self.assertEqual(response.status_code,201)
+        data =json.loads(response.content)
+        self.assertIsNotNone(data['access'])
+        self.assertIsNotNone(data['refresh'])
+
+
+
+
